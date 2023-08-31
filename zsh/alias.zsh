a03d3eeb6adb978ff9fed7f3d985066fcbf6c83a
root <root@SKYNET>
2023-08-31 18:30:45 +0800

    config zsh

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -3,3 +3,7 @@ alias zshconfig="nvim ~/.zshrc"
 alias ohmyzsh="nvim ~/.oh-my-zsh"
 alias bm='bashmount'
 alias ra='ranger'
+alias pv="source $HOME/Document/pyvenv/bin/activate"
+alias dpv="deactivate"
+
+

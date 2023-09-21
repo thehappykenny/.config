491d09a5a8d0d5bf422e8f9c8f60eff33e1cf9d9
root <root@SKYNET>
2023-09-21 23:16:21 +0800

    conda

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -3,7 +3,7 @@ alias zshconfig="nvim ~/.zshrc"
 alias ohmyzsh="nvim ~/.oh-my-zsh"
 alias bm='bashmount'
 alias ra='ranger'
-alias pv="source $HOME/Document/pyvenv/bin/activate"
-alias dpv="deactivate"
-
-
+alias cv="conda activate $HOME/Document/conda_env/cv_3.10/"
+alias dcv="conda deactivate"
+alias cr="clear"
+alias jn="jupyter notebook"
